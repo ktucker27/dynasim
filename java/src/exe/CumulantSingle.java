30c6762715ddc256beb1903bb2903cfd0d758d60
Kris Tucker <ktucker27@gmail.com>
2017-02-19 16:15:00 -0700

    Added CumulantSingle for one-off runs

---
+++ java/src/exe/CumulantSingle.java
@@ -0,0 +1,81 @@
+package exe;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+import handlers.CumulantSteadyStateTerminator;
+import handlers.WriteHandlerCorr;
+import ode.CumulantAllToAllODEs;
+import ode.DynaComplexODEAdapter;
+
+import org.apache.commons.math3.ode.nonstiff.AdamsMoultonIntegrator;
+
+import utils.DynaComplex;
+import utils.SynchUtils;
+
+public class CumulantSingle {
+
+    /**
+     * @param args
+     * @throws UnsupportedEncodingException 
+     * @throws FileNotFoundException 
+     */
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+        int n = 100;
+        double h = 0.005;
+        double gamma = 1.0;
+        double tmax = 10.0;
+        double delta = 0.0;
+        double f = 1.0;
+        double g = 0.0;
+
+        double w = SynchUtils.getW_D0(n);
+        
+        // Get natural frequencies from Gaussian distribution
+        double[] d = new double[n];
+        SynchUtils.detuneGauss(delta, d);
+        
+        DynaComplex alpha = new DynaComplex(f, g);
+        
+        CumulantAllToAllODEs codes = new CumulantAllToAllODEs(n, gamma, w, alpha, d);
+        DynaComplexODEAdapter odes = new DynaComplexODEAdapter(codes);
+        int dim = codes.getDimension();
+        
+        DynaComplex[] z0 = new DynaComplex[dim];
+        SynchUtils.initialize(z0, Math.PI/2.0, n);
+
+        double[] y0 = new double[2*dim];
+        DynaComplexODEAdapter.toReal(z0, y0);
+        
+        double[] y = new double[2*dim];
+        
+        long startTime = System.nanoTime();
+
+        String dir = "/Users/kristophertucker/output/temp/";
+        WriteHandlerCorr writeHandler = new WriteHandlerCorr(dir + "corr_v_time.txt", n);
+        CumulantSteadyStateTerminator term = new CumulantSteadyStateTerminator(1.0, 0.015, 50, 1000000, 0.001, n);
+        AdamsMoultonIntegrator integrator = new AdamsMoultonIntegrator(2, h*1.0e-4, h, 1.0e-3, 1.0e-2);
+        //GraggBulirschStoerIntegrator integrator = new GraggBulirschStoerIntegrator(1.0e-18, h, 1.0e-3, 1.0e-2);
+        //DormandPrince54Integrator integrator = new DormandPrince54Integrator(1.0e-18, h, 1.0e-3, 1.0e-2);
+        //ClassicalRungeKuttaIntegrator integrator = new ClassicalRungeKuttaIntegrator(h);
+        integrator.addStepHandler(writeHandler);
+        integrator.addStepHandler(term.getDetector());
+        integrator.addEventHandler(term, Double.POSITIVE_INFINITY, 1.0e-12, 100);
+
+        System.out.println("w: " + w);
+        integrator.integrate(odes, 0, y0, tmax, y);
+
+        long endTime = System.nanoTime();
+        
+        DynaComplexODEAdapter.toComplex(y, z0);
+        PrintWriter writer = new PrintWriter(dir + "final_answer.txt", "UTF-8");
+        for(int i = 0; i < dim; ++i) {
+            writer.write(z0[i].getReal() + ", " + z0[i].getImaginary() + "\n");
+        }
+        writer.close();
+
+        System.out.println("Run time: " + (endTime - startTime)/1.0e9 + " seconds");
+    }
+
+}
