de6dc665ade78d07eb9f4a5ebc958ff0ae06c745
Kris Tucker <ktucker27@gmail.com>
2017-11-30 19:07:25 -0700

    Added MATLAB routine for computing the matrix associated with the third order cumulant collective two-time correlation equations

---
+++ matlab/master/coll_two_time_matrix_to.m
@@ -0,0 +1,7 @@
+function A = coll_two_time_matrix_to(n, w, f, g, gamma, cz)
+
+A(1,1) = -0.5*(gamma + w) + 0.5*(f - 1i*g)*(n-2)*cz;
+A(1,2) = 0.5*(f - 1i*g)*cz;
+
+A(2,1) = 0.5*(f - 1i*g)*(n - 1)*cz;
+A(2,2) = -0.5*(gamma + w);
