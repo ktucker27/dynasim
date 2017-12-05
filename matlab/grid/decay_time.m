04eee5b5892a9b2bf3633596306c53ef309551f6
Kris Tucker <ktucker27@gmail.com>
2017-12-04 19:24:06 -0700

    Added warning to decay_time when function drops below tolerance close to the end time

--- matlab/grid/decay_time.m
+++ matlab/grid/decay_time.m
@@ -2,3 +2,7 @@ function tc = decay_time(tt, tol)
 
 tcv = tt(abs(tt(:,2)) > tol*abs(tt(1,2)),1);
 tc = tcv(end,1);
+
+if tc > 0.95*tt(end,1)
+    disp('WARNING: Function passed in to decay_time drops below tol within 5% of end time');
+end
