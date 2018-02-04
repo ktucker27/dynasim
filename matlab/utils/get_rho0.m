37d9ad5fbfa89c88ab351329a81fc92854e82ec3
Kris Tucker <ktucker27@gmail.com>
2018-02-03 19:43:44 -0700

    Added matlab function for getting pure state initial density operators

---
+++ matlab/utils/get_rho0.m
@@ -0,0 +1,17 @@
+function rho0 = get_rho0(n, theta, phi)
+
+rand_phase = (nargin < 3);
+
+for i=1:n
+    if rand_phase
+        phi = 2*pi*rand();
+    end
+    
+    v = [cos(theta/2);exp(1i*phi)*sin(theta/2)];
+    rhon = v*v';
+    if i == 1
+        rho0 = rhon;
+    else
+        rho0 = kron(rho0, rhon);
+    end
+end
