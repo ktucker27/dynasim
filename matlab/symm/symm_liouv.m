0516ced0aa11dae33ff4476828c6a1bfc5504e99
Kris Tucker <ktucker27@gmail.com>
2018-06-17 15:54:28 -0600

    Separated onsite and offsite f and g in symm_liouv

--- matlab/symm/symm_liouv.m
+++ matlab/symm/symm_liouv.m
@@ -1,9 +1,11 @@
-function [L, Lf, Sp, Sm, Spr, Smr] = symm_liouv(n, w, o, f, g, gamma, gel)
+function [L, Lfab, Sp, Sm, Spr, Smr] = symm_liouv(n, w, o, faa, fab, gaa, gab, gamma, gel)
 
 d = (n+3)*(n+2)*(n+1)/6;
 
-Lg = zeros(d,d);
-Lf = zeros(d,d);
+Lgaa = zeros(d,d);
+Lgab = zeros(d,d);
+Lfaa = zeros(d,d);
+Lfab = zeros(d,d);
 Lw = zeros(d,d);
 Lel = zeros(d,d);
 
@@ -24,39 +26,41 @@ for j=1:d
         if idx_equals(row_idx, col_idx)
             Lw(i,j) = col_idx(1) + 0.5*(col_idx(2) + col_idx(3));
             %Lf(i,j) = 2*nz*nm + 2*nz + 2*nz*np + np + nm;
-            Lf(i,j) = 0.5*(nm - np)*n1 + 0.5*nz*(nm + 1 + 3*(np + 1)) ...
+            Lfaa(i,j) = col_idx(1) + 0.5*(col_idx(2) + col_idx(3));
+            Lfab(i,j) = 0.5*(nm - np)*n1 + 0.5*nz*(nm + 1 + 3*(np + 1)) ...
                 + 0.5*(n1 + 1)*(np - nm) + 0.5*(nz+1)*(np + 3*nm);
-            Lg(i,j) = col_idx(2) - col_idx(3); % Onsite term
+            Lgaa(i,j) = col_idx(2) - col_idx(3);
             Lel(i,j) = col_idx(2) + col_idx(3);
         end
         
         if idx_equals(row_idx, col_idx + [1;0;0])
-            Lg(i,j) = -(col_idx(2) - col_idx(3))*(col_idx(1) + 1);
+            Lgab(i,j) = -(col_idx(2) - col_idx(3))*(col_idx(1) + 1);
             Lw(i,j) = -(col_idx(1) + 1);
             %Lf(i,j) = nz*nm + 2*nz + nz*np + nm + 2 + np;
-            Lf(i,j) = 0.5*(nz+1)*(nm+1+3*(np+1)) - 0.5*(nz+1)*(np - nm);
+            Lfaa(i,j) = col_idx(1) + 1;
+            Lfab(i,j) = 0.5*(nz+1)*(nm+1+3*(np+1)) - 0.5*(nz+1)*(np - nm);
         end
         
         if idx_equals(row_idx, col_idx - [1;0;0])
-            Lg(i,j) = -(col_idx(2) - col_idx(3))*(n - sum(col_idx) + 1);
+            Lgab(i,j) = -(col_idx(2) - col_idx(3))*(n - sum(col_idx) + 1);
             %Lf(i,j) = n1*nm - n1*np + nm - np;
-            Lf(i,j) = 0.5*(n1+1)*(nm - np) + 0.5*(n1+1)*(nm-np);
+            Lfab(i,j) = 0.5*(n1+1)*(nm - np) + 0.5*(n1+1)*(nm-np);
         end
         
         if idx_equals(row_idx, col_idx + [-1;1;1])
-            Lf(i,j) = -4*(np+1)*(nm+1);
+            Lfab(i,j) = -4*(np+1)*(nm+1);
         end
         
         if idx_equals(row_idx, col_idx + [-2;1;1])
-            Lf(i,j) = -4*(np+1)*(nm+1);
+            Lfab(i,j) = -4*(np+1)*(nm+1);
         end
         
         if idx_equals(row_idx, col_idx + [1;-1;-1])
-            Lf(i,j) = (n1+1)*(nz+1);
+            Lfab(i,j) = (n1+1)*(nz+1);
         end
         
         if idx_equals(row_idx, col_idx + [2;-1;-1])
-            Lf(i,j) = -(nz+1)*(nz+2);
+            Lfab(i,j) = -(nz+1)*(nz+2);
         end
         
         if idx_equals(row_idx, col_idx + [0;1;0])
@@ -105,8 +109,9 @@ for j=1:d
     col_idx = inc_idx(col_idx, n);
 end
 
-%L = -1i*gamma*g/2*Lg - gamma*f/2*Lf - w*Lw;
-L = -1i*gamma*g/2*Lg - 1i*o/2*(Sp + Sm - Spr - Smr) - gamma*f/2*(Sp*Sm + Smr*Spr - 2*Sm*Spr) - w*Lw - gel*Lel;
+%L = -1i*gamma*gaa/2*Lgaa -1i*gamma*gab/2*Lgab - gamma*fab/2*Lf - w*Lw;
+L = -1i*gamma*gaa/2*Lgaa -1i*gamma*gab/2*Lgab - 1i*o/2*(Sp + Sm - Spr - Smr) ...
+       -gamma*fab/2*(Sp*Sm + Smr*Spr - 2*Sm*Spr) - gamma*(faa - fab)*Lfaa - w*Lw - gel*Lel;
 end
 
 function b = idx_equals(idx1, idx2)
