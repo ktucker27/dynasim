cc7909d182a0d28669d3718ce33ee099a2706911
Kris Tucker <ktucker27@gmail.com>
2018-09-17 10:31:33 -0600

    Bug fixes to mcwf.m to get no jump and collective decay dynamics working

--- matlab/qmc/mcwf.m
+++ matlab/qmc/mcwf.m
@@ -1,8 +1,18 @@
-function [tau, rho] = mcwf(n, w, o, faa, fab, chi, gamma, gel, c, t0, dt, t, num_trajectories)
+function [tau, rho, cdfs, outcomes, szs] = mcwf(n, w, o, faa, fab, chi, gamma, gel, c, t0, dt, t, num_trajectories)
 
-J = 1;
+J = n/2;
 
 dim = size(c,1);
+
+sz = zeros(dim, dim);
+
+idx = 1;
+for ji = n/2:-1:-n/2
+    for M = ji:-1:-ji
+        sz(idx, idx) = M;
+        idx = idx + 1;
+    end
+end
 
 gc = gamma*fab;
 gl = gamma*(faa - fab);
@@ -14,12 +24,38 @@ dur = t - t0;
 num_times = ceil(dur/dt);
 tau = zeros(num_times,1);
 rho = zeros(dim, dim, num_times);
+cdfs = zeros(4,num_times-1);
+outcomes = zeros(num_times-1,1);
+
+% Update the density operator
+tau(1,1) = t0;
+
+idx1 = 1;
+for ji = n/2:-1:-n/2
+    jstart = idx1;
+    for M1 = ji:-1:-ji
+        idx2 = jstart;
+        for M2 = ji:-1:-ji
+            rho(idx1, idx2, 1) = rho(idx1, idx2, 1) + c(idx1,1)*c(idx2,1)';
+            idx2 = idx2 + 1;
+        end
+        idx1 = idx1 + 1;
+    end
+end
+
+szs = zeros(num_times,1);
+szs(1,1) = trace(rho(:,:,1)*sz);
+
+cs = zeros(dim, num_trajectories);
+for i = 1:num_trajectories
+    cs(:,i) = c;
+end
 
 % Iterate over time points
-for ti = 1:num_times
-    tau(ti,1) = t0 + ti*dt;
+for ti = 2:num_times
+    tau(ti,1) = t0 + (ti-1)*dt;
     
-    if mod(ti,100) == 0
+    if mod(ti-1,100) == 0
         disp(['t = ', num2str(tau(ti,1))]);
     end
     
@@ -29,8 +65,13 @@ for ti = 1:num_times
         % Update the trajectory
         
         % Determine the jump
-        cdf = get_cdf(c, J, gc, gl, dt);
+        cdf = get_cdf(cs(:,i), n, J, gc, gl, dt);
         outcome = roll_dice(cdf);
+        
+        if i == 1
+            cdfs(:,ti-1) = cdf;
+            outcomes(ti-1,1) = outcome;
+        end
         
         % Update the state coefficients
         newc = zeros(dim,1);
@@ -44,7 +85,7 @@ for ti = 1:num_times
             % Collective decay
             for M = J:-1:-J + 1
                 [~, ajmm] = get_ajm(J,M);
-                newc(idx + 1,1) = sqrt(dt*gc)*ajmm*c(idx,1);
+                newc(idx + 1,1) = sqrt(dt*gc)*ajmm*cs(idx,i);
                 idx = idx + 1;
             end
         elseif outcome == 2
@@ -52,7 +93,7 @@ for ti = 1:num_times
             for M = J:-1:-J
                 [~, ajmm] = get_ajm(J, M);
                 [p0, ~, ~] = get_pjms(n, J, M, ajmm);
-                newc(idx + 1,1) = sqrt(dt*gl)*p0*c(idx,1);
+                newc(idx + 1,1) = sqrt(dt*gl)*p0*cs(idx,i);
                 idx = idx + 1;
             end
         elseif outcome == 3
@@ -66,7 +107,7 @@ for ti = 1:num_times
             for M = J:-1:-J+1
                 [~, ajmm] = get_ajm(J, M);
                 [~, pm, ~] = get_pjms(n, J, M, ajmm);
-                newc(newidx,1) = sqrt(dt*gl)*pm*c(idx,1);
+                newc(newidx,1) = sqrt(dt*gl)*pm*cs(idx,i);
                 idx = idx + 1;
                 newidx = newidx + 1;
             end
@@ -86,7 +127,7 @@ for ti = 1:num_times
             for M = J:-1:-J
                 [~, ajmm] = get_ajm(J, M);
                 [~, ~, pp] = get_pjms(n, J, M, ajmm);
-                newc(newidx,1) = sqrt(dt*gl)*pp*c(idx,1);
+                newc(newidx,1) = sqrt(dt*gl)*pp*cs(idx,i);
                 idx = idx + 1;
                 newidx = newidx + 1;
             end
@@ -98,29 +139,35 @@ for ti = 1:num_times
                 [~, ajmm] = get_ajm(J, M);
                 omjm = oms*ajmm*ajmm ...
                      - 1i*0.5*(gc*ajmm*ajmm + gl*(n/2 + M) + kl*(n/2 - M) + dl*n);
-                newc(idx,1) = (1 - 1i*omjm*dt)*c(idx,1);
+                newc(idx,1) = (1 - 1i*omjm*dt)*cs(idx,i);
                 idx = idx + 1;
             end
         end
-        c = newc;
+        cs(:,i) = newc/norm(newc);
         
         % Update the density operator
-        idx1 = 1;
-        for J = n/2:-1:-n/2
-            jstart = idx1;
-            for M1 = J:-1:-J
-                idx2 = jstart;
-                for M2 = J:-1:-J
-                    rho(idx1, idx2, ti) = rho(idx1, idx2, ti) + c(idx1,1)*c(idx2,1)';
-                    idx2 = idx2 + 1;
-                end
-                idx1 = idx1 + 1;
-            end
-        end
+%         rho_traj = zeros(dim,dim);
+%         idx1 = 1;
+%         for ji = n/2:-1:-n/2
+%             jstart = idx1;
+%             for M1 = ji:-1:-ji
+%                 idx2 = jstart;
+%                 for M2 = ji:-1:-ji
+%                     rho_traj(idx1, idx2) = cs(idx1,i)*cs(idx2,i)';
+%                     rho(idx1, idx2, ti) = rho(idx1, idx2, ti) + cs(idx1,i)*cs(idx2,i)';
+%                     idx2 = idx2 + 1;
+%                 end
+%                 idx1 = idx1 + 1;
+%             end
+%         end
+        
+        %szs(ti,1) = szs(ti,1) + trace(rho_traj*sz);
+        szs(ti,1) = szs(ti,1) + cs(:,i)'*sz*cs(:,i);
     end
     
     % Normalize the density
-    rho(:,:,ti) = rho(:,:,ti)/num_trajectories;
+    %rho(:,:,ti) = rho(:,:,ti)/num_trajectories;
+    szs(ti,1) = real(szs(ti,1)/num_trajectories);
 end
 end
 
@@ -161,16 +208,16 @@ for M = J:-1:-J
     % Individual decay
     [p0, pm, pp] = get_pjms(n, J, M, ajmm);
     cdf(2,1) = cdf(2,1) + abs(p0*cjm)^2;
-    cdf(3,1) = cdf(2,1) + abs(pm*cjm)^2;
-    cdf(4,1) = cdf(2,1) + abs(pp*cjm)^2;
+    cdf(3,1) = cdf(3,1) + abs(pm*cjm)^2;
+    cdf(4,1) = cdf(4,1) + abs(pp*cjm)^2;
 
     idx = idx + 1;
 end
 
 cdf(1,1) = dt*gc*dnj*cdf(1,1);
-cdf(2,1) = dt*gl*dnj*cdf(1,1);
-cdf(3,1) = dt*gl*dnj*cdf(1,1);
-cdf(4,1) = dt*gl*dnj*cdf(1,1);
+cdf(2,1) = dt*gl*dnj*cdf(2,1);
+cdf(3,1) = dt*gl*dnj*cdf(3,1);
+cdf(4,1) = dt*gl*dnj*cdf(4,1);
 
 % Go from probabilities to CDF
 for i=2:size(cdf,1)
