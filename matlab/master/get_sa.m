8a67ba5377f101c32cd77f878718afee7e912d74
Kris Tucker <ktucker27@gmail.com>
2019-03-14 21:47:44 -0600

    Reversed particle order convention in get_sa

--- matlab/master/get_sa.m
+++ matlab/master/get_sa.m
@@ -1,3 +1,4 @@
 function sa = get_sa(s, n, a)
 
-sa = kron(kron(speye(2^(n-a), 2^(n-a)),s), speye(2^(a-1), 2^(a-1)));
+%sa = kron(kron(speye(2^(n-a), 2^(n-a)),s), speye(2^(a-1), 2^(a-1)));
+sa = kron(kron(speye(2^(a-1), 2^(a-1)),s), speye(2^(n-a), 2^(n-a)));
